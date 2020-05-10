d62a5e7c9027a98e91a93ca1d5460474f4841854
Rita Rozsavolgyi <rvrita@Ritas-Air.attlocal.net>
2020-05-09 23:56:27 -0700

    Completing Grocery List project

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryItems={['Kale', 'Cucumber', 'Tomatoes', 'Peppers']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(name => 
+      <GroceryListItem name={name}/>
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor (props) {
+    super(props);
+    // for events always use bind
+    this.onHover = this.onHover.bind(this);
+    this.state = {
+      hover: false
+    };
+  }
+
+  onHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.hover ? 800 : 400
+    };
+    return (
+      <li style={style} onMouseEnter={this.onHover} onMouseLeave={this.onHover}>{this.props.name}</li>
+    );
+  }
+}
+
+ReactDOM.render(< App />, document.getElementById("app"));
